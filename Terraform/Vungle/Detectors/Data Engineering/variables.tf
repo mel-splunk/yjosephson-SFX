8c092cdd0911386dd211b4167a130218fb8ab199
aldridge-s <saldridge@splunk.com>
2019-12-11 10:00:04 -0800

    Update variables.tf

--- Terraform/Vungle/Detectors/Data Engineering/variables.tf
+++ Terraform/Vungle/Detectors/Data Engineering/variables.tf
@@ -1,1 +1,9 @@
+variable "clustersA" {
+    description = "Name of Clusters"
+    default     = "clusterA"
+}
 
+variable "clustersB" {
+    description = "Name of Clusters"
+    default     = "clusterB"
+}
