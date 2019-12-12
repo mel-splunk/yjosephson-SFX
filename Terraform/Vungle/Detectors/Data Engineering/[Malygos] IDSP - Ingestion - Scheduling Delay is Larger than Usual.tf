ebbd6451041c078ccac782406ee3d87e429a92ec
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-12 07:32:52 -0800

    Update [Malygos] IDSP - Ingestion - Scheduling Delay is Larger than Usual.tf

--- Terraform/Vungle/Detectors/Data Engineering/[Malygos] IDSP - Ingestion - Scheduling Delay is Larger than Usual.tf
+++ Terraform/Vungle/Detectors/Data Engineering/[Malygos] IDSP - Ingestion - Scheduling Delay is Larger than Usual.tf
@@ -1,23 +1,19 @@
 # [Malygos] IDSP - Ingestion - Scheduling Delay is Larger than Usual
-resource "signalfx_detector" "[Malygos] IDSP - Ingestion - Scheduling Delay is Larger than Usual" {
-  count = "${length(var.clusters)}"
-  name    = "[Malygos] IDSP - Ingestion - Scheduling Delay is Larger than Usual"
-  description = "Scheduling delay in Malygos IDSP Ingestion streaming job is larger than usual"
+resource "signalfx_detector" "malygos_idsp_ingestion_scheduling_delay_is_larger_than_usual" {
+	count = "1"
+	name = "[Malygos] IDSP - Ingestion - Scheduling Delay is Larger than Usual"
+	description = "Scheduling delay in Malygos IDSP Ingestion streaming job is larger than usual"
 
-/*  query = <<EOQ
-    min(last_1h):avg:malygos.idsp.ingestion.streaming.scheduling.delay.in.seconds{env:prod} / 60 > 10
-  EOQ*/
-
-  program_text = <<-EOF
-      signal = data('malygos.idsp.ingestion.streaming.scheduling.delay.in.seconds', filter=filter('environment', 'prod')).min(over='1h') / 60
-
-			detect(when(signal > 10)).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		signal = data('malygos.idsp.ingestion.streaming.scheduling.delay.in.seconds', filter=filter('environment', 'prod'), rollup='average').min(over='1h') / 60
+		detect(when(signal > 10)).publish('CRIT')
+	EOF
 
 	rule {
-		description = "Min > 10 for last 1h"
+		description = "Min > 10 for last 1b"
 		severity = "Critical"
+		detect_label = "Processing messages last 1h"
+		notifications = ["Email,foo-alerts@bar.com"]
 	}
 
 }
