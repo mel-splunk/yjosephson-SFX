39859ff9fca5172b8dcf5c211e3ac7222fc9b1d6
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-12 07:53:37 -0800

    Update [P1][Malygos] edsp.auctions.mmx failed to run in last 4 hours.tf

--- Terraform/Vungle/Detectors/Data Engineering/[P1][Malygos] edsp.auctions.mmx failed to run in last 4 hours.tf
+++ Terraform/Vungle/Detectors/Data Engineering/[P1][Malygos] edsp.auctions.mmx failed to run in last 4 hours.tf
@@ -1,23 +1,19 @@
 # [P1][Malygos] edsp.auctions.mmx failed to run in last 4 hours
-resource "signalfx_detector" "[P1][Malygos] edsp.auctions.mmx failed to run in last 4 hours" {
-  count = "${length(var.clusters)}"
-  name    = "[P1][Malygos] edsp.auctions.mmx failed to run in last 4 hours"
-  description = "Batch pipeline which consumes data from cheezit edsp_auctions table and perform ETL on them and ship the results to S3 so that MMX could pick them up and show on the MMX dashboard."
+resource "signalfx_detector" "p1_malygos_edsp_auctions_mmx_failed_to_run_in_last_4_hours" {
+	count = "1"
+	name = "[P1][Malygos] edsp.auctions.mmx failed to run in last 4 hours"
+	description = "Batch pipeline which consumes data from cheezit edsp_auctions table and perform ETL on them and ship the results to S3 so that MMX could pick them up and show on the MMX dashboard."
 
-/*  query = <<EOQ
-    sum(last_4h):avg:malygos.edsp_auctions.mmx.success{env:prod} < 1
-  EOQ*/
-
-  program_text = <<-EOF
-      signal = data('malygos.edsp_auctions.mmx.success', filter=filter('environment', 'prod'), rollup='average').sum(over='4h')
-
-			detect(when(signal < 1)).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		signal = data('malygos.edsp_auctions.mmx.success', filter=filter('environment', 'prod'), rollup='average').sum(over='4h')
+		detect(when(signal < 1)).publish('CRIT')
+	EOF
 
 	rule {
 		description = "Sum < 1 for last 4h"
 		severity = "Critical"
+		detect_label = "Processing messages last 4h"
+		notifications = ["Email,foo-alerts@bar.com"]
 	}
 
 }
