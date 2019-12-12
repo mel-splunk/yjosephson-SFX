5890383cf1890f9d011b194d5ef02f2187322a78
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-12 07:50:01 -0800

    Update [P1][Malygos] advertiser_user_events.ingestion report count too low.tf

--- Terraform/Vungle/Detectors/Data Engineering/[P1][Malygos] advertiser_user_events.ingestion report count too low.tf
+++ Terraform/Vungle/Detectors/Data Engineering/[P1][Malygos] advertiser_user_events.ingestion report count too low.tf
@@ -1,23 +1,19 @@
 # [P1][Malygos] advertiser_user_events.ingestion report count too low
-resource "signalfx_detector" "[P1][Malygos] advertiser_user_events.ingestion report count too low" {
-  count = "${length(var.clusters)}"
-  name    = "[P1][Malygos] advertiser_user_events.ingestion report count too low"
-  description = "Streaming pipeline which consumes data from Kafka in-eventDataInfo topic and performs insert into our Cheezit data warehouse for generating advertiser_user_events table."
+resource "signalfx_detector" "p1_malygos_advertiser_user_events_ingestion_report_count_too_low" {
+	count = "1"
+	name = "[P1][Malygos] advertiser_user_events.ingestion report count too low"
+	description = "Streaming pipeline which consumes data from Kafka in-eventDataInfo topic and performs insert into our Cheezit data warehouse for generating advertiser_user_events table."
 
-/*  query = <<EOQ
-    max(last_1h):avg:malygos.advertiser_user_events.ingestion.streaming.consumed.records{env:prod2} < 100000
-  EOQ*/
-
-  program_text = <<-EOF
-      signal = data('malygos.advertiser_user_events.ingestion.streaming.consumed.records', filter=filter('environment', 'prod2'), rollup='average').max(over='1h')
-
-			detect(when(signal < 100000)).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		signal = data('malygos.advertiser_user_events.ingestion.streaming.consumed.records', filter=filter('environment', 'prod2'), rollup='average').max(over='1h')
+		detect(when(signal < 100000)).publish('CRIT')
+	EOF
 
 	rule {
 		description = "Max < 100000 for last 1h"
 		severity = "Critical"
+		detect_label = "Processing messages last 30m"
+		notifications = ["Email,foo-alerts@bar.com"]
 	}
 
 }
