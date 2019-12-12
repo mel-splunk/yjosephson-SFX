9cb579dec89b00afc2301bd6a9a0b8dcad1ea0f5
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-11 16:32:54 -0800

    Update [P1][Malygos] idsp.mmx MetaMarket transaction count too low.tf

--- Terraform/Vungle/Detectors/Data Engineering/[P1][Malygos] idsp.mmx MetaMarket transaction count too low.tf
+++ Terraform/Vungle/Detectors/Data Engineering/[P1][Malygos] idsp.mmx MetaMarket transaction count too low.tf
@@ -1,22 +1,22 @@
-# [P1][Malygos] install postbacks ingestion input count is too low
-resource "signalfx_detector" "[P1][Malygos] install postbacks ingestion input count is too low" {
+# [P1][Malygos] idsp.mmx MetaMarket transaction count too low
+resource "signalfx_detector" "[P1][Malygos] idsp.mmx MetaMarket transaction count too low" {
   count = "${length(var.clusters)}"
-  name    = "[P1][Malygos] install postbacks ingestion input count is too low"
-  description = "Streaming pipeline which loads data from install_postback topic and performs ETL on them and inserts into Cheezit install_postback table. This alert is triggered when the ingested count is lower than the threshold we set."
+  name    = "[P1][Malygos] idsp.mmx MetaMarket transaction count too low"
+  description = "Batch pipeline which loads data from cheezit idsp_transactions table and perform ETL on them and ship the results to S3 vungle-mmx-share/gallywix/mmx bucket so that MMX could pick them up and show the results on their dashboard."
 
 /*  query = <<EOQ
-    max(last_1d):avg:malygos.install_postback.ingestion.input.count{env:prod} < 1
+    max(last_4h):avg:malygos.idsp.mmx.transactions{env:prod} < 1000000
   EOQ*/
 
   program_text = <<-EOF
-      signal = data('malygos.install_postback.ingestion.input.count', filter=filter('environment', 'prod'), rollup='average').max(over='1d')
+      signal = data('malygos.idsp.mmx.transactions', filter=filter('environment', 'prod'), rollup='average').max(over='4h')
 
-			detect(when(signal < 1)).publish('CRIT')
+			detect(when(signal < 1000000)).publish('CRIT')
 
   EOF
 
 	rule {
-		description = "Max < 1 for last 1d"
+		description = "Max < 1000000 for last 4h"
 		severity = "Critical"
 	}
 
