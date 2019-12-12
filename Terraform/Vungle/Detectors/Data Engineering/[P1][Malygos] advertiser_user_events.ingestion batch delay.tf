f16f57184d05a7ee02402f079c96bdb091a853b1
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-12 07:46:58 -0800

    Update [P1][Malygos] advertiser_user_events.ingestion batch delay.tf

--- Terraform/Vungle/Detectors/Data Engineering/[P1][Malygos] advertiser_user_events.ingestion batch delay.tf
+++ Terraform/Vungle/Detectors/Data Engineering/[P1][Malygos] advertiser_user_events.ingestion batch delay.tf
@@ -1,23 +1,19 @@
 # [P1][Malygos] advertiser_user_events.ingestion batch delay
-resource "signalfx_detector" "[P1][Malygos] advertiser_user_events.ingestion batch delay" {
-  count = "${length(var.clusters)}"
-  name    = "[P1][Malygos] advertiser_user_events.ingestion batch delay"
-  description = "Streaming pipeline which consumes data from Kafka in-eventDataInfo topic and performs insert into our Cheezit data warehouse for generating advertiser_user_events table."
+resource "signalfx_detector" "p1_malygos_advertiser_user_events_ingestion_batch_delay" {
+	count = "1"
+	name = "[P1][Malygos] advertiser_user_events.ingestion batch delay"
+	description = "Streaming pipeline which consumes data from Kafka in-eventDataInfo topic and performs insert into our Cheezit data warehouse for generating advertiser_user_events table."
 
-/*  query = <<EOQ
-    max(last_1h):avg:malygos.advertiser_user_events.ingestion.streaming.total.delay.in.seconds{env:prod2} > 2700
-  EOQ*/
-
-  program_text = <<-EOF
-      signal = data('malygos.advertiser_user_events.ingestion.streaming.total.delay.in.seconds', rollup='average').max(over='1h')
-
-			detect(when(signal > 2700)).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		signal = data('malygos.advertiser_user_events.ingestion.streaming.total.delay.in.seconds', rollup='average').max(over='1h')
+		detect(when(signal > 2700)).publish('CRIT')
+	EOF
 
 	rule {
 		description = "Max > 2700 for last 1h"
 		severity = "Critical"
+		detect_label = "Processing messages last 1h"
+		notifications = ["Email,foo-alerts@bar.com"]
 	}
 
 }
