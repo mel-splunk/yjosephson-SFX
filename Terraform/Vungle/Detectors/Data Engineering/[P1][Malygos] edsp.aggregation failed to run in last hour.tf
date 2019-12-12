9a6cc5e5815fdae49ec732985bbf02604e4d6e11
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-12 07:52:06 -0800

    Update [P1][Malygos] edsp.aggregation failed to run in last hour.tf

--- Terraform/Vungle/Detectors/Data Engineering/[P1][Malygos] edsp.aggregation failed to run in last hour.tf
+++ Terraform/Vungle/Detectors/Data Engineering/[P1][Malygos] edsp.aggregation failed to run in last hour.tf
@@ -1,23 +1,19 @@
 # [P1][Malygos] edsp.aggregation failed to run in last hour
-resource "signalfx_detector" "[P1][Malygos] edsp.aggregation failed to run in last hour" {
-  count = "${length(var.clusters)}"
-  name    = "[P1][Malygos] edsp.aggregation failed to run in last hour"
-  description = "Batch pipeline which consumes data from cheezit edsp_transactions table and performs aggregation by mutiple dimensions for several metrics calculation and ship the results into cheezit's publisher_report and edsp_report table."
+resource "signalfx_detector" "p1_malygos_edsp_aggregation_failed_to_run_in_last_hour" {
+	count = "1"
+	name = "[P1][Malygos] edsp.aggregation failed to run in last hour"
+	description = "Batch pipeline which consumes data from cheezit edsp_transactions table and performs aggregation by mutiple dimensions for several metrics calculation and ship the results into cheezit's publisher_report and edsp_report table."
 
-/*  query = <<EOQ
-    sum(last_1h):avg:malygos.edsp.aggregation.success{env:prod} < 1
-  EOQ*/
-
-  program_text = <<-EOF
-      signal = data('malygos.edsp.aggregation.success', filter=filter('environment', 'prod'), rollup='average').sum(over='1h')
-
-			detect(when(signal < 1)).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		signal = data('malygos.edsp.aggregation.success', filter=filter('environment', 'prod'), rollup='average').sum(over='1h')
+		detect(when(signal < 1)).publish('CRIT')
+	EOF
 
 	rule {
 		description = "Sum < 1 for last 1h"
 		severity = "Critical"
+		detect_label = "Processing messages last 1h"
+		notifications = ["Email,foo-alerts@bar.com"]
 	}
 
 }
