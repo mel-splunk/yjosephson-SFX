4ebc9505f91bb7645ed9ccb41a8a8b953427673c
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-12 07:01:46 -0800

    Update [Malygos] EDSP - Aggregation - Account ID for Publisher or Advertiser cannot be found.tf

--- Terraform/Vungle/Detectors/Data Engineering/[Malygos] EDSP - Aggregation - Account ID for Publisher or Advertiser cannot be found.tf
+++ Terraform/Vungle/Detectors/Data Engineering/[Malygos] EDSP - Aggregation - Account ID for Publisher or Advertiser cannot be found.tf
@@ -1,25 +1,21 @@
 # [Malygos] EDSP - Aggregation - Account ID for Publisher or Advertiser cannot be found
-resource "signalfx_detector" "[Malygos] EDSP - Aggregation - Account ID for Publisher or Advertiser cannot be found" {
-  count = "${length(var.clusters)}"
-  name    = "[Malygos] EDSP - Aggregation - Account ID for Publisher or Advertiser cannot be found"
-  description = "Rows in edsp_transactions table do not have matching Account ID"
+resource "signalfx_detector" "malygos_edsp_aggregation_account_id_for_publisher_or_advertiser_cannot_be_found" {
+	count = "1"
+	name = "[Malygos] EDSP - Aggregation - Account ID for Publisher or Advertiser cannot be found"
+	description = "Rows in edsp_transactions table do not have matching Account ID"
 
-/*  query = <<EOQ
-    max(last_1h):avg:malygos.edsp.aggregation.publisherLostAccountId{env:prod} + avg:malygos.edsp.aggregation.edspLostAccountId{env:prod} > 0.5
-  EOQ*/
-
-  program_text = <<-EOF
-      A = data('malygos.edsp.aggregation.publisherLostAccountId', filter=filter('environment', 'prod'))
-      B = data('malygos.edsp.aggregation.edspLostAccountId', filter=filter('environment', 'prod'))
-			
-      signal = (A + B).max(over='1h')
-			detect(when(signal > 0.5)).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		A = data('malygos.edsp.aggregation.publisherLostAccountId', filter=filter('environment', 'prod'))
+		B = data('malygos.edsp.aggregation.edspLostAccountId', filter=filter('environment', 'prod'))
+		signal = (A + B).max(over='1h')
+		detect(when(signal > 0.5)).publish('CRIT')
+	EOF
 
 	rule {
 		description = "Average > 0.5 for last 1h"
 		severity = "Critical"
+		detect_label = "Processing messages last 1h"
+		notifications = ["Email,foo-alerts@bar.com"]
 	}
 
 }
