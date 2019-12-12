9f59cff68ca773512f4d93f7ed391d83a27107c8
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-12 06:41:08 -0800

    Update [Goblin] Mongo2Redshift - Failed to sync table.tf

--- Terraform/Vungle/Detectors/Data Engineering/[Goblin] Mongo2Redshift - Failed to sync table.tf
+++ Terraform/Vungle/Detectors/Data Engineering/[Goblin] Mongo2Redshift - Failed to sync table.tf
@@ -1,14 +1,12 @@
 # [Goblin] Mongo2Redshift - Failed to sync table
 resource "signalfx_detector" "goblin_mongo2Redshift_failed_to_sync_table" {
-  count       = "1"
-  name        = "[Goblin] Mongo2Redshift - Failed to sync table"
+  count = "1"
+  name = "[Goblin] Mongo2Redshift - Failed to sync table"
   description = "The last attempt to sync MongoDB collection to Redshift Cheezit dimension tables saw failure. Failure to sync table between Mongo and Redshift detected earlier has recovered"
 
   program_text = <<-EOF
-      signal = data('cheezit.mongo2redshift.failed_tables', rollup='average').min(over='1h')
-			
-			detect(when(signal > 1 )).publish('CRIT')
-
+    signal = data('cheezit.mongo2redshift.failed_tables', rollup='average').min(over='1h')
+    detect(when(signal > 1 )).publish('CRIT')
   EOF
 
   rule {
