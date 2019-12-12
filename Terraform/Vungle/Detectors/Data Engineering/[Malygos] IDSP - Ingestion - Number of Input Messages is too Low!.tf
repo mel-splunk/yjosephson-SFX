54566e36cacaaf755ee9fdc108339974f77bdf5d
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-12 07:14:31 -0800

    Update [Malygos] IDSP - Ingestion - Number of Input Messages is too Low!.tf

--- Terraform/Vungle/Detectors/Data Engineering/[Malygos] IDSP - Ingestion - Number of Input Messages is too Low!.tf
+++ Terraform/Vungle/Detectors/Data Engineering/[Malygos] IDSP - Ingestion - Number of Input Messages is too Low!.tf
@@ -1,23 +1,19 @@
 # [Malygos] IDSP - Ingestion - Number of Input Messages is too Low!
-resource "signalfx_detector" "[Malygos] IDSP - Ingestion - Number of Input Messages is too Low!" {
-  count = "${length(var.clusters)}"
-  name    = "[Malygos] IDSP - Ingestion - Number of Input Messages is too Low!"
-  description = "Number of messages ingested much lower than normal."
+resource "signalfx_detector" "malygos_idsp_ingestion_number_of_input_messages_is_too_low" {
+	count = "1"
+	name = "[Malygos] IDSP - Ingestion - Number of Input Messages is too Low!"
+	description = "Number of messages ingested much lower than normal."
 
-/*  query = <<EOQ
-    max(last_1h):avg:malygos.idsp.ingestion.streaming.consumed.records{env:prod} < 1000000
-  EOQ*/
-
-  program_text = <<-EOF
-      signal = data('malygos.idsp.ingestion.streaming.consumed.records', filter=filter('environment', 'prod')).max(over='1h')
-      
-			detect(when(signal < 1000000)).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		signal = data('malygos.idsp.ingestion.streaming.consumed.records', filter=filter('environment', 'prod'), rollup='average').max(over='1h')
+		detect(when(signal < 1000000)).publish('CRIT')
+	EOF
 
 	rule {
 		description = "Max < 1000000 for last 1h"
 		severity = "Critical"
+		detect_label = "Processing messages last 1h"
+		notifications = ["Email,foo-alerts@bar.com"]
 	}
 
 }
