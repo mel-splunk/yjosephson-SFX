c353048d12da79fa622a7b313e9df996e42f412b
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-12 07:11:27 -0800

    Update [Malygos] IDSP - Aggregation - Publisher report count is too low.tf

--- Terraform/Vungle/Detectors/Data Engineering/[Malygos] IDSP - Aggregation - Publisher report count is too low.tf
+++ Terraform/Vungle/Detectors/Data Engineering/[Malygos] IDSP - Aggregation - Publisher report count is too low.tf
@@ -1,23 +1,19 @@
 # [Malygos] IDSP - Aggregation - Publisher report count is too low
-resource "signalfx_detector" "[Malygos] IDSP - Aggregation - Publisher report count is too low" {
-  count = "${length(var.clusters)}"
-  name    = "[Malygos] IDSP - Aggregation - Publisher report count is too low"
-  description = "Last IDSP aggregation processed only"
+resource "signalfx_detector" "malygos_idsp_aggregation_publisher_report_count_is_too_low" {
+	count = "1"
+	name = "[Malygos] IDSP - Aggregation - Publisher report count is too low"
+	description = "Last IDSP aggregation processed only"
 
-/*  query = <<EOQ
-    max(last_2h):avg:malygos.idsp.aggregation.publisherReportCount{env:prod} < 50000
-  EOQ*/
-
-  program_text = <<-EOF
-      signal = data('malygos.idsp.aggregation.publisherReportCount', filter=filter('environment', 'prod')).max(over='2h')
-      
-			detect(when(signal < 50000)).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		signal = data('malygos.idsp.aggregation.publisherReportCount', filter=filter('environment', 'prod'), rollup='average').max(over='2h')
+		detect(when(signal < 50000)).publish('CRIT')
+	EOF
 
 	rule {
 		description = "Max < 50000 for last 2h"
 		severity = "Critical"
+		detect_label = "Processing messages last 2h"
+		notifications = ["Email,foo-alerts@bar.com"]
 	}
 
 }
