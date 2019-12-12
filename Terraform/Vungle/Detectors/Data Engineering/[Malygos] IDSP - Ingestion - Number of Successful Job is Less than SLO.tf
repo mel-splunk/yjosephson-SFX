7cb080eae214b65db28be254f5fc41a5b6226dd0
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-12 07:20:25 -0800

    Update [Malygos] IDSP - Ingestion - Number of Successful Job is Less than SLO.tf

--- Terraform/Vungle/Detectors/Data Engineering/[Malygos] IDSP - Ingestion - Number of Successful Job is Less than SLO.tf
+++ Terraform/Vungle/Detectors/Data Engineering/[Malygos] IDSP - Ingestion - Number of Successful Job is Less than SLO.tf
@@ -1,23 +1,19 @@
 # [Malygos] IDSP - Ingestion - Number of Successful Job is Less than SLO
-resource "signalfx_detector" "[Malygos] IDSP - Ingestion - Number of Successful Job is Less than SLO" {
-  count = "${length(var.clusters)}"
-  name    = "[Malygos] IDSP - Ingestion - Number of Successful Job is Less than SLO"
-  description = "Number of successful job run is less than SLO."
+resource "signalfx_detector" "malygos_idsp_ingestion_number_of_successful_job_is_less_than_slo" {
+	count = "1"
+	name = "[Malygos] IDSP - Ingestion - Number of Successful Job is Less than SLO"
+	description = "Number of successful job run is less than SLO."
 
-/*  query = <<EOQ
-    sum(last_1m):max:custom.atf.malygos.idsp.ingestion.success{*} >= 10
-  EOQ*/
-
-  program_text = <<-EOF
-      signal = data('malygos.idsp.ingestion.success', rollup='max').sum(over='1m')
-      
-			detect(when(signal >= 10)).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		signal = data('malygos.idsp.ingestion.success', rollup='max').sum(over='1m')
+		detect(when(signal >= 10)).publish('CRIT')
+	EOF
 
 	rule {
 		description = "Sum >= 10 for last 1m"
 		severity = "Critical"
+		detect_label = "Processing messages last 1m"
+		notifications = ["Email,foo-alerts@bar.com"]
 	}
 
 }
