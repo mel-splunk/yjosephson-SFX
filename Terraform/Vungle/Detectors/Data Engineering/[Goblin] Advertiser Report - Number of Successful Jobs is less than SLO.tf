a6d2ceedd9ba850dbe74b699740da9213a45468f
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-12 09:29:51 -0800

    Update [Goblin] Advertiser Report - Number of Successful Jobs is less than SLO.tf

--- Terraform/Vungle/Detectors/Data Engineering/[Goblin] Advertiser Report - Number of Successful Jobs is less than SLO.tf
+++ Terraform/Vungle/Detectors/Data Engineering/[Goblin] Advertiser Report - Number of Successful Jobs is less than SLO.tf
@@ -1,23 +1,19 @@
 # [Goblin] Advertiser Report - Number of Successful Jobs is less than SLO
-resource "signalfx_detector" "[Goblin] Advertiser Report - Number of Successful Jobs is less than SLO" {
-  count = "${length(var.clusters)}"
-  name    = "[Goblin] Advertiser Report - Number of Successful Jobs is less than SLO"
-  description = "## Description:\nBatch Pipeline which sync data from Cheezit advertiser_report table to Memsql advertiser_report tables and perform aggregations to generate advertiser_report_daily data. The advertiser_report_daily table is source of pacing used by platform. Delay of the data syncing may result in overspend. This alert is triggered when sync records very few, or the redshift2memsql job didn't run for 1 hour."
+resource "signalfx_detector" "goblin_advertiser_report_number_of_successful_jobs_is_less_than_slo" {
+	count = "1"
+	name = "[Goblin] Advertiser Report - Number of Successful Jobs is less than SLO"
+	description = "Batch Pipeline which sync data from Cheezit advertiser_report table to Memsql advertiser_report tables and perform aggregations to generate advertiser_report_daily data."
 
-/*  query = <<EOQ
-    sum(last_1m):max:custom_atf_data_goblin_adv_report_redshift2memsql_prod{*} >= 10
-  EOQ*/
-
-  program_text = <<-EOF
-      signal = data('goblin.redshift2memsql.advertiser_report_daily.rows', rollup='max').sum(over='1m')
-			
-	detect(when(signal >= 10 )).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		signal = data('goblin.redshift2memsql.advertiser_report_daily.rows', rollup='max').sum(over='1m')
+		detect(when(signal >= 10 )).publish('CRIT')
+	EOF
 
 	rule {
 		description = "maximum >= 10 for last 1m"
 		severity = "Critical"
+		detect_label = "Processing messages last 1m"
+		notifications = ["Email,foo-alerts@bar.com"]
 	}
 
 }
