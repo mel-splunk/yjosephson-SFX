e1dc74084234fd1a60476264b2771866fff4b127
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-12 08:00:37 -0800

    Update [P1][Malygos] idsp.mmx MetaMarket transaction count too low.tf

--- Terraform/Vungle/Detectors/Data Engineering/[P1][Malygos] idsp.mmx MetaMarket transaction count too low.tf
+++ Terraform/Vungle/Detectors/Data Engineering/[P1][Malygos] idsp.mmx MetaMarket transaction count too low.tf
@@ -1,23 +1,19 @@
 # [P1][Malygos] idsp.mmx MetaMarket transaction count too low
-resource "signalfx_detector" "[P1][Malygos] idsp.mmx MetaMarket transaction count too low" {
-  count = "${length(var.clusters)}"
-  name    = "[P1][Malygos] idsp.mmx MetaMarket transaction count too low"
-  description = "Batch pipeline which loads data from cheezit idsp_transactions table and perform ETL on them and ship the results to S3 vungle-mmx-share/gallywix/mmx bucket so that MMX could pick them up and show the results on their dashboard."
+resource "signalfx_detector" "p1_malygos_idsp_mmx_metamarket_transaction_count_too_low" {
+	count = "1"
+	name = "[P1][Malygos] idsp.mmx MetaMarket transaction count too low"
+	description = "Batch pipeline which loads data from cheezit idsp_transactions table and perform ETL on them and ship the results to S3 vungle-mmx-share/gallywix/mmx bucket so that MMX could pick them up and show the results on their dashboard."
 
-/*  query = <<EOQ
-    max(last_4h):avg:malygos.idsp.mmx.transactions{env:prod} < 1000000
-  EOQ*/
-
-  program_text = <<-EOF
-      signal = data('malygos.idsp.mmx.transactions', filter=filter('environment', 'prod'), rollup='average').max(over='4h')
-
-			detect(when(signal < 1000000)).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		signal = data('malygos.idsp.mmx.transactions', filter=filter('environment', 'prod'), rollup='average').max(over='4h')
+		detect(when(signal < 1000000)).publish('CRIT')
+	EOF
 
 	rule {
 		description = "Max < 1000000 for last 4h"
 		severity = "Critical"
+		detect_label = "Processing messages last 4h"
+		notifications = ["Email,foo-alerts@bar.com"]
 	}
 
 }
