fb4726b746312046de49f4c90ea752cd4d7449ca
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-12 10:11:57 -0800

    Create [P2][Goblin] Sync cpa_report(rows) encounter error.tf

---
+++ Terraform/Vungle/Detectors/Data Engineering/[P2][Goblin] Sync cpa_report(rows) encounter error.tf
@@ -0,0 +1,19 @@
+# [P2][Goblin] Sync cpa_report(rows) encounter error
+resource "signalfx_detector" "p2_goblin_sync_cpa_report_rows_encounter_error" {
+	count = "1"
+	name = "[P2][Goblin] Sync cpa_report(rows) encounter error"
+	description = "Batch pipeline which sync data from Cheezit cpa_report table to memsql cpa_report table. The table are used by Medivh."
+
+	program_text = <<-EOF
+		signal = data('goblin.redshift2memsql.sync_cpa_report.yesterday.rows', rollup='average').min(over='2h')
+		detect(when(signal < 0)).publish('CRIT')
+	EOF
+
+	rule {
+		description = "Min < 0 for last 2h"
+		severity = "Critical"
+		detect_label = "Processing messages last 2h"
+		notifications = ["Email,foo-alerts@bar.com"]
+	}
+
+}
