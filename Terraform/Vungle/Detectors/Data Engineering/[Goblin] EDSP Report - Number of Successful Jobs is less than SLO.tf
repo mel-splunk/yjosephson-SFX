23a89a6708ef4185610b63ee4821b0be677e680b
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-12 09:35:51 -0800

    Update [Goblin] EDSP Report - Number of Successful Jobs is less than SLO.tf

--- Terraform/Vungle/Detectors/Data Engineering/[Goblin] EDSP Report - Number of Successful Jobs is less than SLO.tf
+++ Terraform/Vungle/Detectors/Data Engineering/[Goblin] EDSP Report - Number of Successful Jobs is less than SLO.tf
@@ -1,23 +1,19 @@
 # [Goblin] EDSP Report - Number of Successful Jobs is less than SLO
-resource "signalfx_detector" "[Goblin] EDSP Report - Number of Successful Jobs is less than SLO" {
-  count = "${length(var.clusters)}"
-  name    = "[Goblin] EDSP Report - Number of Successful Jobs is less than SLO"
-  description = "## Description:\nA batch pipeline which sync data from Cheezit edsp_report table to Memsql edsp_report_column table. The table is used for Medivh queries. This alert is triggered when synced records count less than the threshold, or the job didn't run for 2 hours."
+resource "signalfx_detector" "goblin_edsp_report_number_of_successful_jobs_is_less_than_slo" {
+	count = "1"
+	name = "[Goblin] EDSP Report - Number of Successful Jobs is less than SLO"
+	description = "Batch pipeline which sync data from Cheezit edsp_report table to Memsql edsp_report_column table. The table is used for Medivh queries."
 
-/*  query = <<EOQ
-    sum(last_1m):max:custom_atf_data_goblin_edsp_report_redshift2memsql_prod{*} >= 10
-  EOQ*/
-
-  program_text = <<-EOF
-      signal = data('goblin.redshift2memsql.edsp_report_column.rows', rollup='max').sum(over='1m')
-			
-			detect(when(signal >= 10 )).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		signal = data('goblin.redshift2memsql.edsp_report_column.rows', rollup='max').sum(over='1m')
+		detect(when(signal >= 10 )).publish('CRIT')
+	EOF
 
 	rule {
-		description = "maximum >= 10 for last 1m"
+		description = "Sum >= 10 for last 1m"
 		severity = "Critical"
+		detect_label = "Processing messages last 1m"
+		notifications = ["Email,foo-alerts@bar.com"]
 	}
 
 }
