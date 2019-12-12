f4183fb0b00475821c0a50913352622b8126286e
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-12 07:07:06 -0800

    Update [Malygos] EDSP Auctions - Ingestion - Number of Successful Job is Less than SLO.tf

--- Terraform/Vungle/Detectors/Data Engineering/[Malygos] EDSP Auctions - Ingestion - Number of Successful Job is Less than SLO.tf
+++ Terraform/Vungle/Detectors/Data Engineering/[Malygos] EDSP Auctions - Ingestion - Number of Successful Job is Less than SLO.tf
@@ -1,23 +1,19 @@
 # [Malygos] EDSP Auctions - Ingestion - Number of Successful Job is Less than SLO
-resource "signalfx_detector" "[Malygos] EDSP Auctions - Ingestion - Number of Successful Job is Less than SLO" {
-  count = "${length(var.clusters)}"
-  name    = "[Malygos] EDSP Auctions - Ingestion - Number of Successful Job is Less than SLO"
-  description = "Number of successful job run is less than SLO."
+resource "signalfx_detector" "malygos_edsp_auctions_ingestion_number_of_successful_job_is_less_than_slo" {
+	count = "1"
+	name = "[Malygos] EDSP Auctions - Ingestion - Number of Successful Job is Less than SLO"
+	description = "Number of successful job run is less than SLO."
 
-/*  query = <<EOQ
-    sum(last_1m):max:custom.atf.malygos.edsp_auctions.ingestion.success{*} + 30 >= 40
-  EOQ*/
-
-  program_text = <<-EOF
-      signal = data('malygos.edsp_auctions.ingestion.success', rollup='max').min(over='1m') + 30
-  
-			detect(when(signal >= 40)).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		signal = data('malygos.edsp_auctions.ingestion.success', rollup='max').min(over='1m') + 30
+		detect(when(signal >= 40)).publish('CRIT')
+	EOF
 
 	rule {
 		description = "Min >= 40 for last 1m"
 		severity = "Critical"
+		detect_label = "Processing messages last 1m"
+		notifications = ["Email,foo-alerts@bar.com"]
 	}
 
 }
