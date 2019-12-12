ea6748db3c6567dfc2428e09978a3a39882a9be7
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-12 09:42:19 -0800

    Update [Goblin] LTV Report - Number of Successful Jobs is less than SLO.tf

--- Terraform/Vungle/Detectors/Data Engineering/[Goblin] LTV Report - Number of Successful Jobs is less than SLO.tf
+++ Terraform/Vungle/Detectors/Data Engineering/[Goblin] LTV Report - Number of Successful Jobs is less than SLO.tf
@@ -1,23 +1,19 @@
 # [Goblin] LTV Report - Number of Successful Jobs is less than SLO
-resource "signalfx_detector" "[Goblin] LTV Report - Number of Successful Jobs is less than SLO" {
-  count = "${length(var.clusters)}"
-  name    = "[Goblin] LTV Report - Number of Successful Jobs is less than SLO"
-  description = "## Description:\nMonitor the times for ltv report running which calculate and load data from cheezit to memsql, and then ingest into memsql ltv_report_daily"
+resource "signalfx_detector" "goblin_ltv_report_number_of_successful_jobs_is_less_than_slo" {
+	count = "1"
+	name = "[Goblin] LTV Report - Number of Successful Jobs is less than SLO"
+	description = "Monitor the times for ltv report running which calculate and load data from cheezit to memsql, and then ingest into memsql ltv_report_daily"
 
-/*  query = <<EOQ
-    sum(last_1m):max:custom_atf_data_goblin_calc_ltv_report_prod{*} >= 60
-  EOQ*/
-
-  program_text = <<-EOF
-      signal = data('goblin.ltv_report.count', rollup='max').sum(over='1m')
-			
-			detect(when(signal >= 60 )).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		signal = data('goblin.ltv_report.count', rollup='max').sum(over='1m')
+		detect(when(signal >= 60 )).publish('CRIT')
+	EOF
 
 	rule {
-		description = "maximum >= 60 for last 1m"
+		description = "Sum >= 60 for last 1m"
 		severity = "Critical"
+		detect_label = "Processing messages last 1m"
+		notifications = ["Email,foo-alerts@bar.com"]
 	}
 
 }
