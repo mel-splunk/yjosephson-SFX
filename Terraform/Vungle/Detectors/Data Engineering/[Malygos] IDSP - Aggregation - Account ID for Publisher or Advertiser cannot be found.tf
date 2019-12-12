a7e8b94cc3f2e0303567818e7e9bb72ee6b96b2c
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-12 07:09:23 -0800

    Update [Malygos] IDSP - Aggregation - Account ID for Publisher or Advertiser cannot be found.tf

--- Terraform/Vungle/Detectors/Data Engineering/[Malygos] IDSP - Aggregation - Account ID for Publisher or Advertiser cannot be found.tf
+++ Terraform/Vungle/Detectors/Data Engineering/[Malygos] IDSP - Aggregation - Account ID for Publisher or Advertiser cannot be found.tf
@@ -1,25 +1,21 @@
 # [Malygos] IDSP - Aggregation - Account ID for Publisher or Advertiser cannot be found
-resource "signalfx_detector" "[Malygos] IDSP - Aggregation - Account ID for Publisher or Advertiser cannot be found" {
-  count = "${length(var.clusters)}"
-  name    = "[Malygos] IDSP - Aggregation - Account ID for Publisher or Advertiser cannot be found"
-  description = "Rows in idsp_transactions table do not have matching Account ID"
+resource "signalfx_detector" "malygos_idsp_aggregation_account_id_for_publisher_or_advertiser_cannot_be_found" {
+	count = "1"
+	name = "[Malygos] IDSP - Aggregation - Account ID for Publisher or Advertiser cannot be found"
+	description = "Rows in idsp_transactions table do not have matching Account ID"
 
-/*  query = <<EOQ
-    max(last_1h):avg:malygos.idsp.aggregation.publisherLostAccountId{env:prod} + avg:malygos.idsp.aggregation.advertiserLostAccountId{env:prod} > 100
-  EOQ*/
-
-  program_text = <<-EOF
-      A = data('malygos.idsp.aggregation.publisherLostAccountId', filter=filter('environment', 'prod'))
-      B = data('malygos.idsp.aggregation.advertiserLostAccountId', filter=filter('environment', 'prod'))
-  
-      signal = (A+B).max(over='1h')
-			detect(when(signal > 100)).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		A = data('malygos.idsp.aggregation.publisherLostAccountId', filter=filter('environment', 'prod'))
+		B = data('malygos.idsp.aggregation.advertiserLostAccountId', filter=filter('environment', 'prod'))
+		signal = (A+B).max(over='1h')
+		detect(when(signal > 100)).publish('CRIT')
+	EOF
 
 	rule {
 		description = "Max > 100 for last 1h"
 		severity = "Critical"
+		detect_label = "Processing messages last 1h"
+		notifications = ["Email,foo-alerts@bar.com"]
 	}
 
 }
