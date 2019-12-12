52758fb79e736e0699de65ad041cd2cd31cb987a
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-12 06:48:41 -0800

    Update [Goblin] Redshift to Memsql - Sync advertiser report processing too little data.tf

--- Terraform/Vungle/Detectors/Data Engineering/[Goblin] Redshift to Memsql - Sync advertiser report processing too little data.tf
+++ Terraform/Vungle/Detectors/Data Engineering/[Goblin] Redshift to Memsql - Sync advertiser report processing too little data.tf
@@ -1,23 +1,19 @@
 # [Goblin] Redshift to Memsql - Sync advertiser report processing too little data
-resource "signalfx_detector" "[Goblin] Redshift to Memsql - Sync advertiser report processing too little data" {
-  count = "${length(var.clusters)}"
-  name    = "[Goblin] Redshift to Memsql - Sync advertiser report processing too little data"
-  description = "Sync advertiser report processing too little data"
+resource "signalfx_detector" "goblin_redshift_to_memsql_sync_advertiser_report_processing_too_little_data" {
+	count = "1"
+	name = "[Goblin] Redshift to Memsql - Sync advertiser report processing too little data"
+	description = "Sync advertiser report processing too little data"
 
-/*  query = <<EOQ
-    avg(last_1h):avg:goblin.prod.redshift2memsql.advertiser_report_column.rows{*} < 50000
-  EOQ*/
-
-  program_text = <<-EOF
-      signal = data('goblin.redshift2memsql.advertiser_report_column.rows', rollup='average').mean(over='1h')
-			
-			detect(when(signal < 50000)).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		signal = data('goblin.redshift2memsql.advertiser_report_column.rows', rollup='average').mean(over='1h')
+		detect(when(signal < 50000)).publish('CRIT')
+	EOF
 
 	rule {
 		description = "Count < 50000 for last 1h"
 		severity = "Critical"
+		detect_label  = "Processing messages for last 1h"
+		notifications = ["Email,foo-alerts@bar.com"]
 	}
 
 }
