2bb5a069ea9c5780e6268ac9e7e52f6950b5c487
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-12 07:57:39 -0800

    Update [P1][Malygos] edsp_auctions.ingestion failed to run in 30 minutes.tf

--- Terraform/Vungle/Detectors/Data Engineering/[P1][Malygos] edsp_auctions.ingestion failed to run in 30 minutes.tf
+++ Terraform/Vungle/Detectors/Data Engineering/[P1][Malygos] edsp_auctions.ingestion failed to run in 30 minutes.tf
@@ -1,23 +1,19 @@
 # [P1][Malygos] edsp_auctions.ingestion failed to run in 30 minutes
-resource "signalfx_detector" "[P1][Malygos] edsp_auctions.ingestion failed to run in 30 minutes" {
-  count = "${length(var.clusters)}"
-  name    = "[P1][Malygos] edsp_auctions.ingestion failed to run in 30 minutes"
-  description = "Streaming pipeline which consumes data from kafka ex-jaeger-auction and as-reportAds topics and perform join on them and ship the results to cheezit edsp_auctions table. This table is used for further auction related business analytics."
+resource "signalfx_detector" "p1_malygos_edsp_auctions_ingestion_failed_to_run_in_30_minutes" {
+	count = "1"
+	name = "[P1][Malygos] edsp_auctions.ingestion failed to run in 30 minutes"
+	description = "Streaming pipeline which consumes data from kafka ex-jaeger-auction and as-reportAds topics and perform join on them and ship the results to cheezit edsp_auctions table. This table is used for further auction related business analytics."
 
-/*  query = <<EOQ
-    sum(last_30m):avg:malygos.edsp_auctions.ingestion.success{env:prod} < 1
-  EOQ*/
-
-  program_text = <<-EOF
-      signal = data('malygos.edsp_auctions.ingestion.success', filter=filter('environment', 'prod'), rollup='average').sum(over='30m')
-
-			detect(when(signal < 1)).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		signal = data('malygos.edsp_auctions.ingestion.success', filter=filter('environment', 'prod'), rollup='average').sum(over='30m')
+		detect(when(signal < 1)).publish('CRIT')
+	EOF
 
 	rule {
 		description = "Sum < 1 for last 30m"
 		severity = "Critical"
+		etect_label = "Processing messages last 1h"
+		notifications = ["Email,foo-alerts@bar.com"]
 	}
 
 }
