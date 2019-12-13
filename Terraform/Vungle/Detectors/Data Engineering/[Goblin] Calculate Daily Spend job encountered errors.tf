c645bd902a2b646dd20560f0026bcdcfec1be4cd
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-12 19:22:32 -0800

    Update [Goblin] Calculate Daily Spend job encountered errors.tf

--- Terraform/Vungle/Detectors/Data Engineering/[Goblin] Calculate Daily Spend job encountered errors.tf
+++ Terraform/Vungle/Detectors/Data Engineering/[Goblin] Calculate Daily Spend job encountered errors.tf
@@ -1,18 +1,17 @@
 # [Goblin] Calculate Daily Spend job encountered errors
 resource "signalfx_detector" "goblin_calculate_daily_spend_job_encountered_errors" {
-	count = "1"
 	name = "[Goblin] Calculate Daily Spend job encountered errors"
-	description = "*What happened*\n Error encountered when trying to move dailySpent report data from MemSQL to MongoDB \nNo more new error encountered when trying to move dailySpent report data from MemSQL to MongoDB"
+	description = "Error encountered when trying to move dailySpent report data from MemSQL to MongoDB"
 
 	program_text = <<-EOF
-  		signal = data('goblin.dailySpent.mongodb.errors').mean(by=['sf_metric']).max(over='2h')
+  		signal = data('goblin.dailySpent.mongodb.errors', rollup='average').max(over='2h')
 		detect(when(signal > 1)).publish('CRIT')
 	EOF
 
 	rule {
 		description = "maximum > 1 for last 2hr"
 		severity = "Critical"
-		detect_label = "Processing messages last 2hr"
+		detect_label = "CRIT"
 		notifications = ["Email,foo-alerts@bar.com"]
 	}
 
