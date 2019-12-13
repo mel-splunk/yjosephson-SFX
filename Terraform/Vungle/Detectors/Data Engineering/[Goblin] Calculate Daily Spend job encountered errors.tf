d8cf77f7f8c48144ea0eab4c3212cf652f11cecc
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-13 04:54:03 -0800

    Update [Goblin] Calculate Daily Spend job encountered errors.tf

--- Terraform/Vungle/Detectors/Data Engineering/[Goblin] Calculate Daily Spend job encountered errors.tf
+++ Terraform/Vungle/Detectors/Data Engineering/[Goblin] Calculate Daily Spend job encountered errors.tf
@@ -4,9 +4,11 @@ resource "signalfx_detector" "goblin_calculate_daily_spend_job_encountered_error
 	description = "Error encountered when trying to move dailySpent report data from MemSQL to MongoDB"
 
 	program_text = <<-EOF
-  		signal = data('goblin.dailySpent.mongodb.errors', rollup='average').max(over='2h')
+		signal = data('goblin.dailySpent.mongodb.errors', rollup='average').max(over='2h')
 		detect(when(signal > 1)).publish('CRIT')
 	EOF
+
+	teams = var.team_id
 
 	rule {
 		description = "maximum > 1 for last 2hr"
