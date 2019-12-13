1d69a5d9a93bf59f2e342f7fb8b757efdc7e639b
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-13 14:41:46 -0800

    Create [P2][Malygos] edsp.mmx MetaMarket transactions count too low.tf

---
+++ Terraform/Vungle/Detectors/Platform SF/[P2][Malygos] edsp.mmx MetaMarket transactions count too low.tf
@@ -0,0 +1,20 @@
+# [P2][Malygos] edsp.mmx MetaMarket transactions count too low
+resource "signalfx_detector" "p2_malygos_edsp_mmx_metamarket_transactions_count_too_low" {
+	name = "[P2][Malygos] edsp.mmx MetaMarket transactions count too low"
+	description = "Batch pipeline which consumes data from Cheezit edsp_transactions table and performs ETL on them and ships the results to S3 so that MMX could pick them up and show them on the MMX dashboard"
+
+	program_text = <<-EOF
+		A = data('malygos.edsp.mmx.transactions', filter=filter('environment', 'prod'), rollup='average').max(over='2h')
+		detect(when(signal < 5000)).publish('CRIT')
+	EOF
+
+	teams = var.team_id
+
+	rule {
+		description = "Max < 5000 for last 2h"
+		severity = "Critical"
+		detect_label = "CRIT"
+		notifications = ["Email,foo-alerts@bar.com"]
+	}
+
+}
