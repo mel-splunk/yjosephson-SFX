e5d52a3439c0d5d60a500403db98826eaf91df57
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-13 14:44:01 -0800

    Update [P1][Malygos] edsp.aggregation failed to run in last hour.tf

--- Terraform/Vungle/Detectors/Platform SF/[P1][Malygos] edsp.aggregation failed to run in last hour.tf
+++ Terraform/Vungle/Detectors/Platform SF/[P1][Malygos] edsp.aggregation failed to run in last hour.tf
@@ -3,7 +3,7 @@ resource "signalfx_detector" "p1_malygos_edsp_aggregation_failed_to_run_in_last_
 	name = "[P1][Malygos] edsp.aggregation failed to run in last hour"
 	description = "Batch pipeline which consumes data from cheezit edsp_transactions table and performs aggregation by mutiple dimensions for several metrics calculation and ship the results into cheezit's publisher_report and edsp_report table"
 
-program_text = <<-EOF
+	program_text = <<-EOF
 		A = data('malygos.edsp.aggregation.success', filter=filter('environment', 'prod'), rollup='average').sum(over='1h')
 		detect(when(signal < 1)).publish('CRIT')
 	EOF
