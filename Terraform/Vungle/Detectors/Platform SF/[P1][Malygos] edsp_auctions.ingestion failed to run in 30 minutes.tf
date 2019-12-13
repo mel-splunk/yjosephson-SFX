0700635eefebcf84cf58e659a6b4d1ced1845146
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-13 14:44:35 -0800

    Update [P1][Malygos] edsp_auctions.ingestion failed to run in 30 minutes.tf

--- Terraform/Vungle/Detectors/Platform SF/[P1][Malygos] edsp_auctions.ingestion failed to run in 30 minutes.tf
+++ Terraform/Vungle/Detectors/Platform SF/[P1][Malygos] edsp_auctions.ingestion failed to run in 30 minutes.tf
@@ -3,7 +3,7 @@ resource "signalfx_detector" "p1_malygos_edsp_auctions_ingestion_failed_to_run_i
 	name = "[P1][Malygos] edsp_auctions.ingestion failed to run in 30 minutes"
 	description = "Streaming pipeline which consumes data from kafka ex-jaeger-auction and as-reportAds topics and perform join on them and ship the results to cheezit edsp_auctions table"
 
-program_text = <<-EOF
+	program_text = <<-EOF
 		A = data('malygos.edsp_auctions.ingestion.success', filter=filter('environment', 'prod'), rollup='average').sum(over='30m')
 		detect(when(signal < 1)).publish('CRIT')
 	EOF
