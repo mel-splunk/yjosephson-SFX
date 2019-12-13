99126e80ec752093b8cbe80779db4d5bd85ab4bb
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-13 04:48:50 -0800

    Update [Goblin] Advertiser Report - Number of Successful Jobs is less than SLO.tf

--- Terraform/Vungle/Detectors/Data Engineering/[Goblin] Advertiser Report - Number of Successful Jobs is less than SLO.tf
+++ Terraform/Vungle/Detectors/Data Engineering/[Goblin] Advertiser Report - Number of Successful Jobs is less than SLO.tf
@@ -1,6 +1,5 @@
 # [Goblin] Advertiser Report - Number of Successful Jobs is less than SLO
 resource "signalfx_detector" "goblin_advertiser_report_number_of_successful_jobs_is_less_than_slo" {
-	count = "1"
 	name = "[Goblin] Advertiser Report - Number of Successful Jobs is less than SLO"
 	description = "Batch Pipeline which sync data from Cheezit advertiser_report table to Memsql advertiser_report tables and perform aggregations to generate advertiser_report_daily data."
 
@@ -9,10 +8,12 @@ resource "signalfx_detector" "goblin_advertiser_report_number_of_successful_jobs
 		detect(when(signal >= 10 )).publish('CRIT')
 	EOF
 
+	teams = var.team_id
+
 	rule {
 		description = "maximum >= 10 for last 1m"
 		severity = "Critical"
-		detect_label = "Processing messages last 1m"
+		detect_label = "CRIT"
 		notifications = ["Email,foo-alerts@bar.com"]
 	}
 
