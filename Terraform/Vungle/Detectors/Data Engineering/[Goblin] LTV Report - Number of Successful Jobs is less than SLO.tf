a0708fb70253ab1b605de4881aecc6690f2c9217
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-13 04:55:43 -0800

    Update [Goblin] LTV Report - Number of Successful Jobs is less than SLO.tf

--- Terraform/Vungle/Detectors/Data Engineering/[Goblin] LTV Report - Number of Successful Jobs is less than SLO.tf
+++ Terraform/Vungle/Detectors/Data Engineering/[Goblin] LTV Report - Number of Successful Jobs is less than SLO.tf
@@ -1,6 +1,5 @@
 # [Goblin] LTV Report - Number of Successful Jobs is less than SLO
 resource "signalfx_detector" "goblin_ltv_report_number_of_successful_jobs_is_less_than_slo" {
-	count = "1"
 	name = "[Goblin] LTV Report - Number of Successful Jobs is less than SLO"
 	description = "Monitor the times for ltv report running which calculate and load data from cheezit to memsql, and then ingest into memsql ltv_report_daily"
 
@@ -9,10 +8,12 @@ resource "signalfx_detector" "goblin_ltv_report_number_of_successful_jobs_is_les
 		detect(when(signal >= 60 )).publish('CRIT')
 	EOF
 
+	teams = var.team_id
+
 	rule {
 		description = "Sum >= 60 for last 1m"
 		severity = "Critical"
-		detect_label = "Processing messages last 1m"
+		detect_label = "CRIT"
 		notifications = ["Email,foo-alerts@bar.com"]
 	}
 
