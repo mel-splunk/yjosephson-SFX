a4800b2af0c4a31e01137fc0152cefd0e1213c98
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-13 05:00:25 -0800

    Update [Goblin] Mongo2Redshift - Failed to sync table.tf

--- Terraform/Vungle/Detectors/Data Engineering/[Goblin] Mongo2Redshift - Failed to sync table.tf
+++ Terraform/Vungle/Detectors/Data Engineering/[Goblin] Mongo2Redshift - Failed to sync table.tf
@@ -1,19 +1,20 @@
 # [Goblin] Mongo2Redshift - Failed to sync table
 resource "signalfx_detector" "goblin_mongo2redshift_failed_to_sync_table" {
-  count = "1"
-  name = "[Goblin] Mongo2Redshift - Failed to sync table"
-  description = "The last attempt to sync MongoDB collection to Redshift Cheezit dimension tables saw failure. Failure to sync table between Mongo and Redshift detected earlier has recovered"
+	name = "[Goblin] Mongo2Redshift - Failed to sync table"
+	description = "The last attempt to sync MongoDB collection to Redshift Cheezit dimension tables saw failure. Failure to sync table between Mongo and Redshift detected earlier has recovered"
 
-  program_text = <<-EOF
-    signal = data('cheezit.mongo2redshift.failed_tables', rollup='average').min(over='1h')
-    detect(when(signal > 1 )).publish('CRIT')
-  EOF
+	program_text = <<-EOF
+		signal = data('cheezit.mongo2redshift.failed_tables', rollup='average').min(over='1h')
+		detect(when(signal > 1 )).publish('CRIT')
+	EOF
 
-  rule {
-    description   = "Min > 1 for last 1hr"
-    severity      = "Critical"
-    detect_label  = "Processing messages last 1hr"
-    notifications = ["Email,foo-alerts@bar.com"]
-  }
+	teams = var.team_id
+
+	rule {
+		description   = "Min > 1 for last 1hr"
+		severity      = "Critical"
+		detect_label  = "CRIT"
+		notifications = ["Email,foo-alerts@bar.com"]
+	}
 
 }
