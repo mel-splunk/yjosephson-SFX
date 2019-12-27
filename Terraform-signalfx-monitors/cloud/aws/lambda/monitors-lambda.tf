9175b1322968f648e98b3771afb5e2e0c3592371
Yo <50105712+ybjosephson@users.noreply.github.com>
2019-12-27 10:31:27 -0800

    Update monitors-lambda.tf

--- Terraform-signalfx-monitors/cloud/aws/lambda/monitors-lambda.tf
+++ Terraform-signalfx-monitors/cloud/aws/lambda/monitors-lambda.tf
@@ -1,104 +1,70 @@
 # Errors Percent
 resource "signalfx_detector" "pct_errors" {
-  count   = var.pct_errors_enabled == "true" ? 1 : 0
-  name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] Lambda Percentage of errors {{#is_alert}}{{{comparator}}} {{threshold}}% ({{value}}%){{/is_alert}}{{#is_warning}}{{{comparator}}} {{warn_threshold}}% ({{value}}%){{/is_warning}}"
+	name = "Lambda Percentage of errors"
 
-/*  query = <<EOQ
-    ${var.pct_errors_time_aggregator}(${var.pct_errors_timeframe}):
-      default(
-        (default(sum:aws.lambda.errors${module.filter-tags.query_alert} by {region,functionname}.as_count(),0)
-        /
-        default(sum:aws.lambda.invocations${module.filter-tags.query_alert} by {region,functionname}.as_count(),1))
-        * 100,0)
-      > ${var.pct_errors_threshold_critical}
-  EOQ*/
+	program_text = <<-EOF
+		A = data('Errors', filter=filter('namespace', 'AWS/Lambda'), rollup='count', extrapolation='zero').sum(by=['aws_region', 'FunctionName'])
+		B = data('Invocations', filter=filter('namespace', 'AWS/Lambda'), rollup='count').sum(by=['aws_region', 'FunctionName'])
+		signal = (A / B).scale(100).sum(over='1h')
+		detect(when(signal > 30)).publish('CRIT')
 
-  program_text = <<-EOF
-      A = data('aws.lambda.errors', rollup='sum').${var.pct_errors_time_aggregator}.sum(by=['aws_region','aws_function_name']).count(by=['aws_region'])
-			B = data('aws.lambda.invocation', rollup='sum').${var.pct_errors_time_aggregator}.sum(by=['aws_region','aws_function_name']).count(by=['aws_region'])
-		  signal = (A / B) * 100
-			detect(when(signal > ${var.pct_errors_threshold_critical}, max('${var.pct_errors_timeframe}'))).publish('CRIT')
-
-  EOF
+	EOF
 
 	rule {
-		description = coalesce(var.pct_errors_message, var.message)
+		description = "Sum > 30 for last 1h"
 		severity = "Critical"
+		detect_label = "CRIT"
 	}
 
 }
 
 # Errors Absolute Value
 resource "signalfx_detector" "errors" {
-  count   = var.errors_enabled == "true" ? 1 : 0
-  name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] Lambda Number of errors {{#is_alert}}{{{comparator}}} {{threshold}} ({{value}}){{/is_alert}}{{#is_warning}}{{{comparator}}} {{warn_threshold}} ({{value}}){{/is_warning}}"
+	name = "Lambda Number of errors"
 
- /* query = <<EOQ
-    ${var.errors_time_aggregator}(${var.errors_timeframe}):
-      default(sum:aws.lambda.errors${module.filter-tags.query_alert} by {region,functionname}.as_count(),0)
-      > ${var.errors_threshold_critical}
-  EOQ*/
-
-  program_text = <<-EOF
-      signal = data('aws.lambda.errors', rollup='sum').${var.errors_time_aggregator}.sum(by=['aws_region','aws_function_name']).count(by=['aws_region'])
-			
-			detect(when(signal > ${var.errors_threshold_critical}, max('${var.errors_timeframe}'))).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		signal = data('Errors', filter=filter('namespace', 'AWS/Lambda'), rollup='count', extrapolation='zero').sum(by=['aws_region', 'FunctionName']).sum(over='1h')
+		detect(when(signal > 3)).publish('CRIT')
+	EOF
 
 	rule {
-		description = coalesce(var.errors_message, var.message)
+		description = "Sum > 3 for last 1h"
 		severity = "Critical"
+		detect_label = "CRIT"
 	}
 
 }
 
 # Throttles
 resource "signalfx_detector" "throttles" {
-  count   = var.throttles_enabled == "true" ? 1 : 0
-  name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] Lambda Invocations throttled due to concurrent limit reached {{#is_alert}}{{{comparator}}} {{threshold}} ({{value}}){{/is_alert}}{{#is_warning}}{{{comparator}}} {{warn_threshold}} ({{value}}){{/is_warning}}"
+	name = "Lambda Invocations throttled due to concurrent limit reached"
 
-  /*query = <<EOQ
-    ${var.throttles_time_aggregator}(${var.throttles_timeframe}):
-      default(sum:aws.lambda.throttles${module.filter-tags.query_alert} by {region,functionname}.as_count(),0)
-      > ${var.throttles_threshold_critical}
-  EOQ*/
-
-  program_text = <<-EOF
-      signal = data('aws.lambda.throttles', rollup='sum').${var.throttles_time_aggregator}.sum(by=['aws_region','aws_function_name']).count(by=['aws_region'])
-			
-			detect(when(signal > ${var.throttles_critical}, max('${var.throttles_timeframe}'))).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		signal = data('Throttles', filter=filter('namespace', 'AWS/Lambda'), rollup='count', extrapolation='zero').sum(by=['aws_region', 'FunctionName']).sum(over='1h')
+		detect(when(signal > 3)).publish('CRIT')
+	EOF
 
 	rule {
-		description = coalesce(var.throttles_message, var.message)
+		description = "Sum > 3 for last 1h"
 		severity = "Critical"
+		detect_label = "CRIT"
 	}
 
 }
 
 # INVOCATIONS
 resource "signalfx_detector" "invocations" {
-  count   = var.invocations_enabled == "true" ? 1 : 0
-  name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] Lambda Number of invocations {{#is_alert}}{{{comparator}}} {{threshold}} ({{value}}){{/is_alert}}{{#is_warning}}{{{comparator}}} {{warn_threshold}} ({{value}}){{/is_warning}}"
+	name = "Lambda Number of invocations"
 
-  /*query = <<EOQ
-    ${var.invocations_time_aggregator}(${var.invocations_timeframe}):
-      default(sum:aws.lambda.invocations${module.filter-tags.query_alert} by {region,functionname}.as_count(),0)
-      <= ${var.invocations_threshold_critical}
-  EOQ*/
-
-  program_text = <<-EOF
-      signal = data('aws.lambda.invocations', rollup='sum').${var.invocations_time_aggregator}.sum(by=['aws_region','aws_function_name']).count(by=['aws_region'])
-			
-			detect(when(signal <= ${var.invocations_critical}, max('${var.invocations_timeframe}'))).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		signal = data('invocations', filter=filter('namespace', 'AWS/Lambda'), rollup='count', extrapolation='zero').sum(by=['aws_region', 'FunctionName']).sum(over='30m')
+		detect(when(signal <= 1)).publish('CRIT')
+	EOF
 
 	rule {
-		description = coalesce(var.invocations_message, var.message)
+		description = "Sum <= 1 for last 30m"
 		severity = "Critical"
+		detect_label = "CRIT"
 	}
 
 }
