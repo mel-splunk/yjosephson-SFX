507b08b75413cd83b47ff5de8210d3e43f581c02
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-01-13 12:17:33 -0800

    Update monitors-api.tf

--- Terraform-signalfx-monitors/cloud/aws/apigateway/monitors-api.tf
+++ Terraform-signalfx-monitors/cloud/aws/apigateway/monitors-api.tf
@@ -1,82 +1,54 @@
 # Monitoring Api Gateway latency
-resource "signalfx_monitor" "API_Gateway_latency" {
-  count   = var.latency_enabled == "true" ? 1 : 0
-  name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] API Gateway latency {{#is_alert}}{{{comparator}}} {{threshold}}ms ({{value}}ms){{/is_alert}}{{#is_warning}}{{{comparator}}} {{warn_threshold}}ms ({{value}}ms){{/is_warning}}"
-  message = coalesce(var.latency_message, var.message)
-  type    = "query alert"
+resource "signalfx_detector" "API_Gateway_latency" {
+	name = "API Gateway latency"
 
-  /*query = <<EOQ
-    ${var.latency_time_aggregator}(${var.latency_timeframe}):
-      default(avg:aws.apigateway.latency{${var.filter_tags}} by {region,apiname,stage}, 0)
-    > ${var.latency_threshold_critical}
-  EOQ*/
-
-  program_text = <<-EOF
-      signal = data('Latency', rollup='mean').${var.latency_time_aggregator}.mean(by=['aws_region','namespace'])
-			
-      detect(when(signal > ${var.latency_threshold_critical}, max('${var.latency_timeframe}'))).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		signal = data('Latency', filter=filter('namespace', 'AWS/ApiGateway'), extrapolation='zero').mean(by=['aws_region','ApiName', 'Stage']).min(over='5m')
+		detect(when(signal > 3000)).publish('CRIT')
+	EOF
 
 	rule {
-		description = coalesce(var.latency_message, var.message)
+		description = "Min > 3000 for last 5m"
 		severity = "Critical"
+		detect_label = "CRIT"
 	}
 
 }
 
 # Monitoring API Gateway 5xx errors percent
-resource "signalfx_monitor" "API_http_5xx_errors_count" {
-  count   = var.http_5xx_requests_enabled == "true" ? 1 : 0
-  name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] API Gateway HTTP 5xx errors {{#is_alert}}{{{comparator}}} {{threshold}}% ({{value}}%){{/is_alert}}{{#is_warning}}{{{comparator}}} {{warn_threshold}}% ({{value}}%){{/is_warning}}"
+resource "signalfx_detector" "API_http_5xx_errors_count" {
+	name = "API Gateway HTTP 5xx errors"
 
-  /*query = <<EOQ
-    ${var.http_5xx_requests_time_aggregator}(${var.http_5xx_requests_timeframe}):
-      default(avg:aws.apigateway.5xxerror{${var.filter_tags}} by {region,apiname,stage}.as_rate(), 0) / (
-      default(avg:aws.apigateway.count{${var.filter_tags}} by {region,apiname,stage}.as_rate() + ${var.artificial_requests_count}, 1))
-      * 100 > ${var.http_5xx_requests_threshold_critical}
-  EOQ*/
-
-  program_text = <<-EOF
-      A = data('5XXError', rollup='mean').${var.http_5xx_requests_time_aggregator}.mean(by=['aws_region','namespace'])
-			B = data('Count', rollup='mean').${var.http_5xx_requests_time_aggregator}.mean(by=['aws_region','namespace'])
-      signal = (A/(B + ${var.artificial_requests_count})).scale(100)
-
-      detect(when(signal > ${var.http_5xx_requests_threshold_critical}, max('${var.http_5xx_requests_timeframe}'))).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		A = data('5XXError', filter=filter('namespace', 'AWS/ApiGateway'), extrapolation='zero', rollup='rate').mean(by=['aws_region','ApiName','Stage'])
+		B = data('Count', filter=filter('namespace', 'AWS/ApiGateway'), extrapolation='zero', rollup='rate').mean(by=['aws_region','ApiName','Stage'])
+		signal = (A/(B+5)).scale(100).min(over='5m')
+		detect(when(signal > 20)).publish('CRIT')
+	EOF
 
 	rule {
-		description = coalesce(var.http_5xx_requests_message, var.message)
+		description = "Min > 20 for last 5m"
 		severity = "Critical"
+		detect_label = "CRIT"
 	}
 
 }
 
 # Monitoring API Gateway 4xx errors percent
-resource "signalfx_monitor" "API_http_4xx_errors_count" {
-  count   = var.http_4xx_requests_enabled == "true" ? 1 : 0
-  name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] API Gateway HTTP 4xx errors {{#is_alert}}{{{comparator}}} {{threshold}}% ({{value}}%){{/is_alert}}{{#is_warning}}{{{comparator}}} {{warn_threshold}}% ({{value}}%){{/is_warning}}"
+resource "signalfx_detector" "API_http_4xx_errors_count" {
+	name = "API Gateway HTTP 4xx errors"
 
-  /*query = <<EOQ
-    ${var.http_4xx_requests_time_aggregator}(${var.http_4xx_requests_timeframe}):
-      default(avg:aws.apigateway.4xxerror{${var.filter_tags}} by {region,apiname,stage}.as_rate(), 0) / (
-      default(avg:aws.apigateway.count{${var.filter_tags}} by {region,apiname,stage}.as_rate() + ${var.artificial_requests_count}, 1))
-      * 100 > ${var.http_4xx_requests_threshold_critical}
-  EOQ*/
-
-  program_text = <<-EOF
-      A = data('4XXError', rollup='mean').${var.http_4xx_requests_time_aggregator}.mean(by=['aws_region','namespace'])
-			B = data('Count', rollup='mean').${var.http_4xx_requests_time_aggregator}.mean(by=['aws_region','namespace'])
-      signal = (A/(B + ${var.artificial_requests_count})).scale(100)
-
-      detect(when(signal > ${var.http_4xx_requests_threshold_critical}, max('${var.http_4xx_requests_timeframe}'))).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		A = data('4XXError', filter=filter('namespace', 'AWS/ApiGateway'), extrapolation='zero', rollup='rate').mean(by=['aws_region','ApiName','Stage'])
+		B = data('Count', filter=filter('namespace', 'AWS/ApiGateway'), extrapolation='zero', rollup='rate').mean(by=['aws_region','ApiName','Stage'])
+		signal = (A/(B+5)).scale(100).min(over='5m')
+		detect(when(signal > 30)).publish('CRIT')
+	EOF
 
 	rule {
-		description = coalesce(var.http_4xx_requests_message, var.message)
+		description = "Min > 30 for last 5m"
 		severity = "Critical"
+		detect_label = "CRIT"
 	}
 
 }
