d124aaffb2c9c19673e5df7412dc3ba84c9cb701
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-01-15 17:27:40 -0800

    Update monitors-lambda.tf

--- Terraform-signalfx-monitors/cloud/aws/lambda/monitors-lambda.tf
+++ Terraform-signalfx-monitors/cloud/aws/lambda/monitors-lambda.tf
@@ -3,11 +3,10 @@ resource "signalfx_detector" "pct_errors" {
 	name = "Lambda Percentage of errors"
 
 	program_text = <<-EOF
-		A = data('Errors', filter=filter('namespace', 'AWS/Lambda'), rollup='count', extrapolation='zero').sum(by=['aws_region', 'FunctionName'])
-		B = data('Invocations', filter=filter('namespace', 'AWS/Lambda'), rollup='count').sum(by=['aws_region', 'FunctionName'])
+		A = data('Errors', filter=filter('namespace', 'AWS/Lambda'), rollup='sum', extrapolation='zero').sum(by=['aws_region', 'FunctionName'])
+		B = data('Invocations', filter=filter('namespace', 'AWS/Lambda'), rollup='sum').sum(by=['aws_region', 'FunctionName'])
 		signal = (A / B).scale(100).sum(over='1h')
 		detect(when(signal > 30)).publish('CRIT')
-
 	EOF
 
 	rule {
@@ -23,7 +22,7 @@ resource "signalfx_detector" "errors" {
 	name = "Lambda Number of errors"
 
 	program_text = <<-EOF
-		signal = data('Errors', filter=filter('namespace', 'AWS/Lambda'), rollup='count', extrapolation='zero').sum(by=['aws_region', 'FunctionName']).sum(over='1h')
+		signal = data('Errors', filter=filter('namespace', 'AWS/Lambda'), rollup='sum', extrapolation='zero').sum(by=['aws_region', 'FunctionName']).sum(over='1h')
 		detect(when(signal > 3)).publish('CRIT')
 	EOF
 
@@ -40,7 +39,7 @@ resource "signalfx_detector" "throttles" {
 	name = "Lambda Invocations throttled due to concurrent limit reached"
 
 	program_text = <<-EOF
-		signal = data('Throttles', filter=filter('namespace', 'AWS/Lambda'), rollup='count', extrapolation='zero').sum(by=['aws_region', 'FunctionName']).sum(over='1h')
+		signal = data('Throttles', filter=filter('namespace', 'AWS/Lambda'), rollup='sum', extrapolation='zero').sum(by=['aws_region', 'FunctionName']).sum(over='1h')
 		detect(when(signal > 3)).publish('CRIT')
 	EOF
 
@@ -57,7 +56,7 @@ resource "signalfx_detector" "invocations" {
 	name = "Lambda Number of invocations"
 
 	program_text = <<-EOF
-		signal = data('invocations', filter=filter('namespace', 'AWS/Lambda'), rollup='count', extrapolation='zero').sum(by=['aws_region', 'FunctionName']).sum(over='30m')
+		signal = data('invocations', filter=filter('namespace', 'AWS/Lambda'), rollup='sum', extrapolation='zero').sum(by=['aws_region', 'FunctionName']).sum(over='30m')
 		detect(when(signal <= 1)).publish('CRIT')
 	EOF
 
