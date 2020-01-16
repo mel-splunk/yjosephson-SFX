af92c3412bb88e8ba5500ca71c9b6e41cd24ab48
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-01-15 17:24:47 -0800

    Update monitors-kinesis-firehose.tf

--- Terraform-signalfx-monitors/cloud/aws/kinesis-firehose/monitors-kinesis-firehose.tf
+++ Terraform-signalfx-monitors/cloud/aws/kinesis-firehose/monitors-kinesis-firehose.tf
@@ -1,24 +1,16 @@
 ### Kinesis Firehose Incoming records ###
-resource "signalfx_monitor" "firehose_incoming_records" {
-  count   = var.incoming_records_enabled == "true" ? 1 : 0
-  name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] Kinesis Firehose No incoming records"
+resource "signalfx_detector" "firehose_incoming_records" {
+	name = "Kinesis Firehose No incoming records"
 
-  /*query = <<EOQ
-    sum(${var.incoming_records_timeframe}): (
-      avg:aws.firehose.incoming_records${module.filter-tags.query_alert} by {region,deliverystreamname}
-    ) <= 0
-  EOQ*/
-
-  program_text = <<-EOF
-      signal = data('IncomingRecords', filter=filter('namespace', 'AWS/Firehose'), rollup='mean').mean(by=['aws_region','DeliveryStreamName'])
-			
-			detect(when(signal <= 0, ${var.incoming_records_timeframe})).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		signal = data('IncomingRecords', filter=filter('namespace', 'AWS/Kinesis')).mean(by=['aws_region','StreamName']).sum(over='15m')
+		detect(when(signal <= 0)).publish('CRIT')
+	EOF
 
 	rule {
-		description = coalesce(var.incoming_records_message, var.message)
+		description = "Sum <= 0 for last 15m"
 		severity = "Critical"
+		detect_label = "CRIT"
 	}
 
 }
