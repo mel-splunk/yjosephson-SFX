5a985fc05ed7acd7ed4c7ea1efd69f3c4bae3800
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-01-15 17:33:45 -0800

    Update monitors-nlb.tf

--- Terraform-signalfx-monitors/cloud/aws/nlb/monitors-nlb.tf
+++ Terraform-signalfx-monitors/cloud/aws/nlb/monitors-nlb.tf
@@ -1,27 +1,17 @@
-resource "signalfx_monitor" "NLB_no_healthy_instances" {
-  count   = var.nlb_no_healthy_instances_enabled == "true" ? 1 : 0
-  name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] NLB healthy instances {{#is_alert}}is at 0{{/is_alert}}{{#is_warning}}is at {{value}}%%{{/is_warning}}"
+resource "signalfx_detector" "NLB_no_healthy_instances" {
+	name = "NLB healthy instances"
 
-  /*query = <<EOQ
-    ${var.nlb_no_healthy_instances_time_aggregator}(${var.nlb_no_healthy_instances_timeframe}): (
-      sum:aws.networkelb.healthy_host_count.minimum${module.filter-tags.query_alert} by {region,loadbalancer} / (
-      sum:aws.networkelb.healthy_host_count.minimum${module.filter-tags.query_alert} by {region,loadbalancer} +
-      sum:aws.networkelb.un_healthy_host_count.maximum${module.filter-tags.query_alert} by {region,loadbalancer} )
-    ) * 100 < 1
-  EOQ*/
-
-  program_text = <<-EOF
-      A = data('HealthyHostCount', filter=filter('namespace', 'AWS/NetworkELB'), rollup='sum').${var.nlb_no_healthy_instances_time_aggregator}.sum(by=['aws_region','LoadBalancerName'])
-			B = data('UnHealthyHostCount', filter=filter('namespace', 'AWS/NetworkELB'), rollup='sum').${var.nlb_no_healthy_instances_time_aggregator}.sum(by=['aws_region','LoadBalancerName'])
-			
-      signal = (A / (A+B)).scale(100)
-			detect(when(signal < 1 ${var.nlb_no_healthy_instances_timeframe})).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		A = data('HealthyHostCount', filter=filter('namespace', 'AWS/NetworkELB')).sum(by=['aws_region','LoadBalancerName'])
+		B = data('UnHealthyHostCount', filter=filter('namespace', 'AWS/NetworkELB')).sum(by=['aws_region','LoadBalancerName'])
+		signal = (A / (A+B)).scale(100).min(over='5m')
+		detect(when(signal < 1)).publish('CRIT')
+	EOF
 
 	rule {
-		description = coalesce(var.nlb_no_healthy_instances_message, var.message)
+		description = "Min < 1 for last 5m"
 		severity = "Critical"
+		detect_label = "CRIT"
 	}
 
 }
