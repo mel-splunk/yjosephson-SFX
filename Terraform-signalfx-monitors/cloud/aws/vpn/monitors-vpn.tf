7ffcbe5416eeca34317a72604fa362c9d469c3a5
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-01-16 10:52:44 -0800

    Update monitors-vpn.tf

--- Terraform-signalfx-monitors/cloud/aws/vpn/monitors-vpn.tf
+++ Terraform-signalfx-monitors/cloud/aws/vpn/monitors-vpn.tf
@@ -1,26 +1,22 @@
 resource "signalfx_monitor" "VPN_status" {
-  count   = var.vpn_status_enabled == "true" ? 1 : 0
-  name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] VPN tunnel down"
-  message = coalesce(var.vpn_status_message, var.message)
-  type    = "query alert"
+	name = "VPN tunnel down"
 
-  /*query = <<EOQ
-        ${var.vpn_status_time_aggregator}(${var.vpn_status_timeframe}): (
-          min:aws.vpn.tunnel_state{${var.filter_tags}} by {region,tunnelipaddress}
-        ) < 1
-  EOQ*/
+	/*query = <<EOQ
+				${var.vpn_status_time_aggregator}(${var.vpn_status_timeframe}): (
+					min:aws.vpn.tunnel_state{${var.filter_tags}} by {region,tunnelipaddress}
+				) < 1
+	EOQ*/
 
-  program_text = <<-EOF
-    /* Currently SFx doesn't have AWS/VPN so need to revisit this */
-      signal = data('TunnelState', filter=filter('namespace', 'AWS/VPN'), rollup='min').${var.vpn_status_time_aggregator}.min(by=['aws_region','TunnerIPAddress'])
-			
-			detect(when(signal < 1).publish('CRIT')
-
-  EOF
+	program_text = <<-EOF
+		/* Currently SFx doesn't have AWS/VPN so need to revisit this */
+		signal = data('TunnelState', filter=filter('namespace', 'AWS/VPN')).min(by=['aws_region','TunnerIPAddress']).max(over='5m')
+		detect(when(signal < 1)).publish('CRIT')
+	EOF
 
 	rule {
-		description = coalesce(var.vpn_status_message, var.message)
+		description = "Sum > 10 for last 5m"
 		severity = "Critical"
+		detect_label = "CRIT"
 	}
 
 }
