37ec2d7a158094701b64163b52850223f8845956
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-02-25 15:17:40 -0800

    Update monitors-kong.tf

--- Terraform-signalfx-monitors/middleware/kong/monitors-kong.tf
+++ Terraform-signalfx-monitors/middleware/kong/monitors-kong.tf
@@ -1,15 +1,3 @@
-#
-# Service Check
-#
-resource "signalfx_detector" "not_responding" {
-	name    = "Kong does not respond"
-
-	/*query = <<EOQ
-		"kong.can_connect"${module.filter-tags.service_check}.by("kong_host","kong_port").last(6).count_by_status()
-EOQ*/
-
-}
-
 resource "signalfx_detector" "treatment_limit" {
 	name = "Kong exceeded its treatment limit"
 
@@ -17,11 +5,11 @@ resource "signalfx_detector" "treatment_limit" {
 		A = data('counter.kong.connections.handled', filter=filter('plugin', 'kong')).min(by=['host'])
 		B = data('counter.kong.connections.accepted', filter=filter('plugin', 'kong')).min(by=['host'])
 		signal = ((A-B)/A).scale(100).min(over='15m')
-		detect(when(signal > 20)).publish('CRIT')
+		detect(when(signal > ${var.treatment_limit_threshold_critical})).publish('CRIT')
 	EOF
 
 	rule {
-		description = "Min > 20 for last 15m"
+		description = "Min > ${var.treatment_limit_threshold_critical} for last 15m"
 		severity = "Critical"
 		detect_label = "CRIT"
 	}
