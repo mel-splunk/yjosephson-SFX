7d9ca19c5aa2a8d3c412e61bfe0398be37ca647e
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-03-01 09:04:51 -0800

    Update monitors-nginx.tf

--- Terraform-signalfx-monitors/middleware/nginx/monitors-nginx.tf
+++ Terraform-signalfx-monitors/middleware/nginx/monitors-nginx.tf
@@ -1,14 +1,33 @@
-resource "signalfx_detector" "nginx_dropped_connections" {
-	name = "Nginx dropped connections"
+resource "signalfx_detector" "nginx_heartbeat" {
+	name = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] System Heartbeat Check"
 
 	program_text = <<-EOF
-		signal = data('connections.failed', filter=filter('plugin', 'nginx')).mean(by=['host']).min(over='5m')
-		detect(when(signal > ${var.nginx_dropped_threshold_critical})).publish('CRIT')
+		from signalfx.detectors.not_reporting import not_reporting
+		signal = data('nginx_requests', filter=(not filter('aws_state', '*terminated}', '*stopped}')) and (not filter('gcp_status', '*TERMINATED}', '*STOPPING}')) and (not filter('azure_power_state', 'stop*', 'deallocat*')))
+		not_reporting.detector(stream=signal, resource_identifier=['host'], duration='${var.nginx_heartbeat_timeframe}').publish('CRIT')
 	EOF
 
 	rule {
-		description = "Min > ${var.nginx_dropped_threshold_critical} for last 5m"
+		description = "System has not reported in ${var.nginx_heartbeat_timeframe}"
 		severity = "Critical"
 		detect_label = "CRIT"
+		disabled = "${var.nginx_heartbeat_disabled_flag}"
 	}
+}
+
+resource "signalfx_detector" "nginx_dropped_connections" {
+	name = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] Nginx dropped connections"
+
+	program_text = <<-EOF
+		signal = data('connections.failed'"${var.nginx_filter == "" ? "" : "${var.nginx_filter}"}")${var.nginx_aggregation_function}.${var.nginx_transformation_function}(over='${var.nginx_transformation_window}')
+		detect(when(signal > ${var.nginx_threshold_critical})).publish('CRIT')
+	EOF
+
+	rule {
+		description = "${var.nginx_transformation_function} nginx dropped over ${var.nginx_transformation_window} > ${var.nginx_threshold_critical}"
+		severity = "Critical"
+		detect_label = "CRIT"
+		disabled = var.nginx_critical_disabled_flag
+	}
+
 }
