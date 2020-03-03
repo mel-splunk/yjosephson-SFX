a18090bb847f81e66889095b496bae03eb3a1517
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-03-03 06:51:05 -0800

    Update integrations-gcp.tf

--- Terraform-signalfx-integrations/cloud/gcp/integrations-gcp.tf
+++ Terraform-signalfx-integrations/cloud/gcp/integrations-gcp.tf
@@ -1,9 +1,11 @@
-resource "signalfx_gcp_integration" "gcp_claranet" {
-	name = "GCP - Claranet"
-	enabled = true
-	poll_rate = 300
-	project_service_keys {
-		project_id = var.gcp_project_id
-		project_key = var.gcp_project_key
-	}
+resource "signalfx_gcp_integration" "gcp_claranet_integration" {
+  name      = "GCPIntegration${var.suffix == "" ? "" : "-${title(var.suffix)}"}"
+  enabled   = var.enabled
+  poll_rate = var.poll_rate
+  project_service_keys {
+    project_id  = var.gcp_project_id
+    project_key = base64decode(google_service_account_key.sak.private_key)
+  }
+
+  depends_on = [google_project_iam_member.sfx_service_account_membership]
 }
