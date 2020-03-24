f895322f87da7de6e369665fce1bb059c4205b63
Yo <50105712+ybjosephson@users.noreply.github.com>
2020-03-24 06:55:15 -0700

    Delete outputs.tf

--- Terraform-signalfx-integrations/cloud/aws/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "aws_role_arn" {
-  description = "The role ARN of the SignalFx integration"
-  value       = aws_iam_role.sfx_role.arn
-}
-
-output "aws_role_name" {
-  description = "The IAM role name of the SignalFx integration"
-  value       = aws_iam_role.sfx_role.name
-}
